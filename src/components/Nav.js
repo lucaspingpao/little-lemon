4eac6aeb5b3c37acff20cc2babd222f663a39737
Lucas <lucaspingpao@gmail.com>
2024-10-06 02:49:18 -0400

    add semantic tags and metadata

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+function Nav() {
+    return (
+        <nav>
+            <ul>
+                <img src="logo"/>
+                <li><a href="">Home</a></li>
+                <li><a href="">Home</a></li>
+                <li><a href="">Home</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
